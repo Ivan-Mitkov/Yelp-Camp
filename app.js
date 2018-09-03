ff0898a26048a13b00574bd2cf64d465a5a991a8
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-09-03 08:54:46 +0300

    Basic pages

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-const express=require('express');
+const express = require('express');
 
-const app=express();
-let port = process.env.port||3000;
+const app = express();
+let port = process.env.port || 3000;
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('lending');
 });
 
-app.get('campgrouns',(req,res)=>{
-    
+app.get('/campgrounds', (req, res) => {
+    let campgrounds = [{
+        name: 'Sinanitsa',
+        image: "http://www.bulgarian-mountains.com/Huts/Pirin/Sinanica/picture_three.jpg"
+    }, {
+        name: 'Tevno ezero',
+        image: "http://www.bulgarian-mountains.com/Huts/Pirin/TevnoEzero/picture.jpg"
+
+    }, {
+        name: 'Spano pole',
+        image: "http://www.bulgarian-mountains.com/Huts/Pirin/SpanoPole/picture_two.jpg"
+
+    }, {
+        name: 'Vihren',
+        image: "http://www.bulgarian-mountains.com/Huts/Pirin/Vihren/picture_two.jpg"
+
+    }, {
+        name: 'Banderitsa',
+        image: "http://www.bulgarian-mountains.com/Huts/Pirin/Bunderica/picture.jpg"
+
+    }, {
+        name: 'Yavorov',
+        image: "http://www.bulgarian-mountains.com/Huts/Pirin/Iavorov/picture.jpg"
+    }
+    ]
+    res.render('campgrounds',{campgrounds:campgrounds});
 });
 
-app.listen(port,()=>{
-    console.log('Yelp Camp is listening on port:'+port)
+app.listen(port, () => {
+    console.log('Yelp Camp is listening on port:' + port)
 })
