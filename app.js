dd052fc6edfd61764d2db130e960bd79072ce1b7
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-09-06 10:26:49 +0300

    Add mongo for persistence and add description to campgrounds

--- app.js
+++ app.js
@@ -1,54 +1,86 @@
 const express = require('express');
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const app = express();
 let port = process.env.port || 3000;
-let campgrounds = [{
-    name: 'Sinanitsa',
-    image: "http://www.bulgarian-mountains.com/Huts/Pirin/Sinanica/picture_three.jpg"
-}, {
-    name: 'Tevno ezero',
-    image: "http://www.bulgarian-mountains.com/Huts/Pirin/TevnoEzero/picture.jpg"
 
-}, {
-    name: 'Spano pole',
-    image: "http://www.bulgarian-mountains.com/Huts/Pirin/SpanoPole/picture_two.jpg"
 
-}, {
-    name: 'Vihren',
-    image: "http://www.bulgarian-mountains.com/Huts/Pirin/Vihren/picture_two.jpg"
+mongoose.connect("mongodb://localhost/yelp_camp");
 
-}, {
-    name: 'Banderitsa',
-    image: "http://www.bulgarian-mountains.com/Huts/Pirin/Bunderica/picture.jpg"
 
-}, {
-    name: 'Yavorov',
-    image: "http://www.bulgarian-mountains.com/Huts/Pirin/Iavorov/picture.jpg"
-}
-]
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+// SHEMA SET UP
+let campGroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+let CampGround = mongoose.model('Campground', campGroundSchema);
 
 app.get('/', (req, res) => {
     res.render('lending');
 });
 
+//Index rout show all campgrounds
 app.get('/campgrounds', (req, res) => {
-  
-    res.render('campgrounds',{campgrounds:campgrounds});
+    //   get all campground from db
+    CampGround.find({}, (err, allCampgrounds) => {
+        if (err) {
+            console.log('Error retrieving campgrounds');
+        } else {
+            res.render('index', { campgrounds: allCampgrounds });
+        }
+
+    })
+
 });
 
-app.get('/campgrounds/new',(req,res)=>{
+//NEW - show form to create new campground
+app.get('/campgrounds/new', (req, res) => {
     res.render('new.ejs');
 });
-app.post('/campgrounds',(req,res)=>{
-   let formResult=req.body;
-   campgrounds.push(formResult);
-   res.redirect('/campgrounds');
+
+//CREATE rout add new campground to th DB
+app.post('/campgrounds', (req, res) => {
+    let name = req.body.name;
+    console.log(name);
+    let image = req.body.image;
+    console.log(image);
+    let description = req.body.description;
+    console.log(description);
+
+    let newCampGround = { name: name, image: image, description: description };
+    console.log(newCampGround);
+    // Create and save to the DB
+    CampGround.create(newCampGround, (err, newlyCreatedCamp) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds');
+        }
+    })
+
+});
+
+//SHOW show details about one item MUST BE AFTER !!!NEW route
+app.get('/campgrounds/:id', (req, res) => {
    
+    CampGround.findById(req.params.id,function(err,foundCampGround){
+        if(err){
+            console.log(err);
+        }else{
+           
+            res.render('show.ejs',{campground:foundCampGround});
+        }
+    });    
+
 });
 
 app.listen(port, () => {
     console.log('Yelp Camp is listening on port:' + port)
-})
+});
